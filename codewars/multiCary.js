cf13b3c9b7caa30a662c54ef6a77eab48b4ff770
Aleksandr Zubakin <eldoson@yandex.ru>
2020-08-01 23:43:54 +0500

    carry function

---
+++ codewars/multiCary.js
@@ -0,0 +1,8 @@
+/* left blank for unlimited creativity :) */
+const multiplyAll = x => y => {
+   let result = [];
+   for (let i = 0; i < x.length; i++) {
+      result[i] = x[i] * y;
+   }
+   return result;
+}
---
+++ codewars/reverseString.js
@@ -0,0 +1,8 @@
+function solution(str) {
+   let resultStr;
+   for (let i = str.length; i >= 0;)
+      return resultStr;
+}
+
+
+console.log(solution('world'));
